c077d8b7209d3416da7b08cc3ade6d1efd7c7492
Felipe Gabriel <129404898+felipefg4@users.noreply.github.com>
2023-05-23 11:07:54 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+function ativarNome(){
+    let nome = 'filu';
+    console.log(nome) 
+    document.getElementById('nome').innerHTML = nome
+
+}
+
+function somarNumeros(){
+    let a = 7;
+    let b = 3;
+    let c = a/b;
+
+    console.log(c)
+    
+    document.getElementById('aparecer').innerHTML = c
+}
